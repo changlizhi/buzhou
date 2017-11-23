c4614246c95a341af2abd4eb2d183cea3c97f3f8
rootclz <rootclz@localhost.localdomain>
2017-11-23 21:28:46 +0800

    down load gcc but no us

--- 6vmnginx.sh
+++ 6vmnginx.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-#jiaocheng ----- https://www.cnblogs.com/hanyinglong/p/5102141.html
-sudo curl http://prdownloads.sourceforge.net/libpng/zlib-1.2.11.tar.gz -O
+
+# jiaocheng ----- https://www.cnblogs.com/hanyinglong/p/5102141.html
+# butong banben gcc http://blog.sina.com.cn/s/blog_457c4fd10100o5yj.html
+# ruguo xiazai shibai lujing keneng cuole,mulu quanxian yekeneng cuo 
+# ftp name:password de wenti yehui shi lujing wenti
+sudo curl http://mirror.centos.org/centos/7/os/x86_64/Packages/gcc-4.8.5-16.el7.x86_64.rpm -O
+sudo curl http://vault.centos.org/7.3.1611/os/Source/SPackages/gcc-4.8.5-16.el7.src.rpm -O
+sudo curl http://mirrors-usa.go-parts.com/gcc/releases/gcc-4.9.4/gcc-4.9.4.tar.gz -O
+sudo curl http://mirrors-usa.go-parts.com/gcc/releases/gcc-5.5.0/gcc-5.5.0.tar.gz -O
+sudo curl http://mirrors-usa.go-parts.com/gcc/releases/gcc-6.4.0/gcc-6.4.0.tar.gz -O
+sudo crul http://mirrors-usa.go-parts.com/gcc/releases/gcc-7.2.0/gcc-7.2.0.tar.gz -O
+sudo curl http://www.cpan.org/src/5.0/perl-5.26.1.tar.gz -O
+sudo curl https://nchc.dl.sourceforge.net/project/libpng/zlib/1.2.11/zlib-1.2.11.tar.gz -O
 sudo curl ftp://ftp.csx.cam.ac.uk/pub/software/programming/pcre/pcre-8.41.tar.gz -O
 sudo curl https://www.openssl.org/source/openssl-1.0.2m.tar.gz -O
 sudo curl http://nginx.org/download/nginx-1.13.7.tar.gz -O
