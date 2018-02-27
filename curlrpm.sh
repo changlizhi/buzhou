0ecaff26fa86915ec2c2920681f456b2e119ea2a
changlizhi <changlizhi@aliyun.com>
2018-02-26 22:06:56 -0500

    add curl rpm

---
+++ curlrpm.sh
@@ -0,0 +1,7 @@
+https://raw.githubusercontent.com/changlizhi/buzhou/master/python-chardet-2.2.1-1.el7_1.noarch.rpm
+https://raw.githubusercontent.com/changlizhi/buzhou/master/python-kitchen-1.1.1-5.el7.noarch.rpm
+https://raw.githubusercontent.com/changlizhi/buzhou/master/python-urlgrabber-3.10-8.el7.noarch.rpm
+https://raw.githubusercontent.com/changlizhi/buzhou/master/yum-3.4.3-154.el7.centos.noarch.rpm
+https://raw.githubusercontent.com/changlizhi/buzhou/master/yum-metadata-parser-1.1.4-10.el7.x86_64.rpm
+https://raw.githubusercontent.com/changlizhi/buzhou/master/yum-plugin-fastestmirror-1.1.31-42.el7.noarch.rpm
+https://raw.githubusercontent.com/changlizhi/buzhou/master/yum-utils-1.1.31-42.el7.noarch.rpm
